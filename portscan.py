e1ffc5e4ae16f163424b76df0d020540f93bf60d
emmalynn <emmalynn.hoyt@pm.me>
2021-02-28 15:08:12 -0600

    code cleanup, added gitignore

--- portscan.py
+++ portscan.py
@@ -4,7 +4,7 @@ import sys, socket, errno, argparse, re, os.path
 from socket import error as socket_error
 from os import path
 
-# variables and settings
+# variables
 ports = [21, 22, 23, 25, 53, 80, 110, 139, 143, 443, 445, 1433, 1521, 3306, 3389]
 
 # create and set arguments
@@ -17,54 +17,41 @@ parser.add_argument('--version', '-v', action="version", version="%(prog)s 1.0")
 
 # try to connect
 def testconnect():
-	try:
-		server_address = (addr, port)
-		sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		sock.settimeout(timeout)
-		sock.connect(server_address)
-		print("%s open" % port)
-		sock.close()		
-	except socket.timeout:
-		print("%s timeout" % port)
-	except socket_error as serr:
-		if serr.errno == errno.ECONNREFUSED:
-			print("%s closed" % port)
-		if serr.errno != errno.ECONNREFUSED:
-			raise serr
+	print('Host/IP: %s' % addr)
+	# iterate over ports
+	for port in ports:
+		try:
+			server_address = (addr, port)
+			sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			sock.settimeout(timeout)
+			sock.connect(server_address)
+			print("%s open" % port)
+			sock.close()		
+		except socket.timeout:
+			print("%s timeout" % port)
+		except socket_error as serr:
+			if serr.errno == errno.ECONNREFUSED:
+				print("%s closed" % port)
+			if serr.errno != errno.ECONNREFUSED:
+				raise serr
 
 # parse arguments
 args = parser.parse_args()
+timeout = args.timeout
 
 # IPv4 Regex
 aa = re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",args.ipaddress)
 
-
-timeout = args.timeout
-
 # reader either IP or file given
 if aa:
 	addr = args.ipaddress
-	print('Host/IP: %s' % addr)
-	# iterate over ports
-	for port in ports:
-		
-		testconnect()
+	testconnect()
 elif not aa and path.exists(args.ipaddress):
-
 	with open(args.ipaddress) as f:
 		for line in f:
 			parts = line.split()
 			if len(parts) > 1:
 				addr = parts[1]
-
-				print('Host/IP: %s' % addr)
-
-				# iterate over ports
-				for port in ports:
-					testconnect()
-				
-
-
+				testconnect()
 else:
 	sys.exit("Please enter valid input")
-
